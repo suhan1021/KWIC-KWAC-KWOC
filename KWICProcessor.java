a149cad23cccbe13c5a2d6c4003666105bef3eb4
suhan <a0105517@nus.edu.sg>
2014-08-21 00:40:55 +0800

    processor component of KWIC system

---
+++ KWICProcessor.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class KWICProcessor {
+	ArrayList<String> titles;
+	ArrayList<String> wordsToIgnore;
+	ArrayList<String> keywordsInContext;
+
+	public KWICProcessor(String[] titles, String[] wordsToIgnore) {
+		this.titles = new ArrayList<String>(Arrays.asList(titles));
+		this.wordsToIgnore = new ArrayList<String>(Arrays.asList(wordsToIgnore));
+		keywordsInContext = new ArrayList<String>();
+	}
+
+	public ArrayList<String> getKeyWordsInContext() {
+		Collections.sort(wordsToIgnore);
+		findAllKeywords();
+		return keywordsInContext;
+	}
+
+	private void findAllKeywords() {
+		// find all keywords in the give list of titles.
+		for (int i = 0; i < titles.size(); i++) {
+			findKeywordsInOneTitle(titles.get(i));
+		}
+	}
+
+	private void findKeywordsInOneTitle(String title) {
+		// find all keywords in one tile and store in output list.
+		int index = 0;
+		int length = title.split(" ").length;
+		String rest;
+
+		for (int i = 0; i < length; i++) {
+			String first = title.split(" ")[0];
+			rest = title.substring(title.indexOf(" ")).trim();
+			index = Collections
+					.binarySearch(wordsToIgnore, first.toLowerCase());
+			if (index >= 0
+					&& wordsToIgnore.get(index).equals(first.toLowerCase())) {
+				// it is a word to be ignored
+				title = rest + " " + first.toLowerCase(); 
+			} else {
+				// it is a keyword
+				int pos = Collections.binarySearch(keywordsInContext, title);
+				keywordsInContext.add(-pos - 1, title);
+				title = rest + " " + first;
+			}
+		}
+	}
+}
