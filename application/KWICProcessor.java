e0da909e59f4f164a1e9f345f2465c3628b06c5a
suhan <a0105517@nus.edu.sg>
2014-08-30 01:11:08 +0800

    dived processor into more modularized parts

--- application/KWICProcessor.java
+++ application/KWICProcessor.java
@@ -1,53 +1,50 @@
-package application;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 class KWICProcessor {
 	ArrayList<String> titles;
 	ArrayList<String> wordsToIgnore;
 	ArrayList<String> keywordsInContext;
 
-	public KWICProcessor(ArrayList<String> titles, ArrayList<String> wordsToIgnore) {
-		this.titles = titles;
-		this.wordsToIgnore = wordsToIgnore;
+	public KWICProcessor(String[] titles, String[] wordsToIgnore) {
+		this.titles = new ArrayList<String>(Arrays.asList(titles));
+		this.wordsToIgnore = new ArrayList<String>(Arrays.asList(wordsToIgnore));
 		keywordsInContext = new ArrayList<String>();
 	}
 
 	public ArrayList<String> getKeyWordsInContext() {
-		Collections.sort(wordsToIgnore);
 		findAllKeywords();
+		alphabetizeIndex(keywordsInContext);
 		return keywordsInContext;
 	}
 
 	private void findAllKeywords() {
-		// find all keywords in the give list of titles.
 		for (int i = 0; i < titles.size(); i++) {
 			findKeywordsInOneTitle(titles.get(i));
 		}
 	}
 
 	private void findKeywordsInOneTitle(String title) {
-		// find all keywords in one tile and store in output list.
-		int index = 0;
 		int length = title.split(" ").length;
-		String rest;
-
 		for (int i = 0; i < length; i++) {
 			String first = title.split(" ")[0];
-			rest = title.substring(title.indexOf(" ")).trim();
-			index = Collections
-					.binarySearch(wordsToIgnore, first.toLowerCase());
-			if (index >= 0
-					&& wordsToIgnore.get(index).equals(first.toLowerCase())) {
+			int index = wordsToIgnore.indexOf(first.toLowerCase());
+			if (index >= 0) {
 				// it is a word to be ignored
-				title = rest + " " + first.toLowerCase(); 
+				title = circularShift(title, first.toLowerCase());
 			} else {
 				// it is a keyword
-				int pos = Collections.binarySearch(keywordsInContext, title);
-				keywordsInContext.add(-pos - 1, title);
-				title = rest + " " + first;
+				keywordsInContext.add(title);
+				title = circularShift(title, first);
 			}
 		}
+	}
+
+	private String circularShift(String title, String first) {
+		String rest = title.substring(title.indexOf(" ")).trim();
+		return rest + " " + first;
+	}
+
+	private void alphabetizeIndex(ArrayList<String> list) {
+		Collections.sort(list);
 	}
 }
